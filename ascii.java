58388f874b1675fe95845f208ca31e56a883483b
adityadhanraj05 <157284243+adityadhanraj05@users.noreply.github.com>
2024-01-21 17:07:47 +0530

    Add files via upload

---
+++ ascii.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ascii {
+    public static void main(String[] args) {
+        char x;
+        System.out.println("enter a character");
+        Scanner sc=new Scanner(System.in);
+        x=sc.next().charAt(0);
+        //System.out.println(x);
+
+        int a=x;
+        System.out.println(a);
+
+    }
+}
